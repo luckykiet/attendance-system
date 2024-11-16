dc8775d7ba73589a25b2be934fef3717bc2a5da7
Tuan Kiet Nguyen <ngntuankiet@gmail.com>
2024-11-16 16:03:50 +0100

    update config structure

--- admin/src/configs/config.jsx
+++ admin/src/configs/config.jsx
@@ -1,14 +1,19 @@
-const APP_NAME = 'ATTENDANCE SYSTEM Admin'
+const APP_NAME = 'ATTENDANCE SYSTEM Admin';
+const MOBILE_INTENT = 'attendance://';
 
 export const HOSTNAME = window.location.hostname.split('.').slice(-2).join('.');
 export const HOST = HOSTNAME + (window.location.port ? `:${window.location.port}` : '');
+export const PROTOCOL = window.location.protocol + '//';
+export const PROXY_URL = '';
 
 export const CONFIG = {
   APP_NAME: APP_NAME,
   HOSTNAME: HOSTNAME,
   HOST: HOST,
+  PROTOCOL: PROTOCOL,
   JWT_SECRET: '',
   RECAPTCHA_SITE_KEY: '',
   GOOGLE_MAPS_API_KEY: '',
   WEB_PUBLIC_URL: HOSTNAME,
-}
+  MOBILE_INTENT: MOBILE_INTENT,
+};
--- server/configs/config.js
+++ server/configs/config.js
@@ -1,12 +1,14 @@
 const development = process.env.NODE_ENV === 'development';
 const protocol = 'https://';
-const subdomain = 'app.';
+const subdomain = 'app';
 const www = 'www.';
 const realm = 'local';
 const domain = `attendance.${realm}`;
 const admin_subdomain = 'admin';
 const admin_port = '5173'
 const admin_domain = `${admin_subdomain}.${domain}${development ? `:${admin_port}` : ''}`;
+const proxy_domain = development ? '' : '';
+const mobile_intent = 'attendance://';
 
 const CONFIG = {
     protocol: protocol,
@@ -14,9 +16,11 @@ const CONFIG = {
     admin_port: admin_port,
     admin_subdomain: admin_subdomain,
     admin_domain: admin_domain,
+    subdomain: subdomain,
     realm: realm,
-    host: protocol + subdomain + domain,
+    host: proxy_domain ? proxy_domain : `${protocol}${subdomain}.${domain}`,
     www: protocol + www + domain,
+    mobile_intent: mobile_intent,
     mongodb_host: 'mongodb://127.0.0.1:27017/attendance',
     companyName: domain.toUpperCase(),
     appName: 'ATTENDANCE SYSTEM',
