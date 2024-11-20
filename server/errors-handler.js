55506674ff99b3e896b0b36c31100e44f991d784
Tuan Kiet Nguyen <ngntuankiet@gmail.com>
2024-11-20 12:33:45 +0100

    improve error handler

--- server/errors-handler.js
+++ server/errors-handler.js
@@ -10,36 +10,44 @@ const errorLogger = (err, req, res, next) => {
 
 const errorResponder = (err, req, res, next) => {
   if (req.file) {
-    fs.unlink(req.file.path, (err) => {
-      console.log(err)
-    })
+    fs.unlink(req.file.path, (unlinkErr) => {
+      if (unlinkErr) console.error('Failed to delete uploaded file:', unlinkErr);
+    });
   }
 
-  if (res.headerSent) {
-    return next(err)
+  if (res.headersSent) {
+    return next(err);
   }
 
-  res.header('Content-Type', 'application/json')
+  res.header('Content-Type', 'application/json');
 
   if (err.name === 'ValidationError' || err.name === 'ValidatorError') {
     res.status(422).json({
       success: false,
       msg: Object.keys(err.errors).reduce((errors, key) => {
-        errors[key] = 'srv_validation_error'
-        return errors
+        errors[key] = 'srv_validation_error';
+        return errors;
       }, {}),
-    })
+    });
   } else if (err.code === 11000) {
     res.status(409).json({
       success: false,
       msg: 'srv_error',
-    })
+    });
   } else {
+    let errorMsg;
+
+    try {
+      errorMsg = err.message ? JSON.parse(err.message) : 'An unknown error occurred!';
+    } catch (parseError) {
+      errorMsg = parseError instanceof Error ? parseError.message : err.message || 'An unknown error occurred!';
+    }
+
     res.status(err.code || 500).send({
       success: false,
-      msg: err.message ? JSON.parse(err.message) : 'An unknown error occurred!',
-    })
+      msg: errorMsg,
+    });
   }
-}
+};
 
 module.exports = { errorLogger, errorResponder }
