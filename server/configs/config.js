98987e065dfe6dc8fe8e71f9f9facd9a2820c595
Tuan Kiet Nguyen <ngntuankiet@gmail.com>
2025-03-12 11:07:54 +0100

    remove secret

--- server/configs/config.js
+++ server/configs/config.js
@@ -51,25 +51,25 @@ const CONFIG = {
         secure: true,
         auth: {
             type: 'OAuth2',
-            user: 'admin@gokarte.cz',
-            clientId: '113712253408-i8v0a4na1k8govgh3qg7ms6kflp01nim.apps.googleusercontent.com',
-            clientSecret: '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',
-            refreshToken: '1//04SPbeSYOh0BACgYIARAAGAQSNwF-L9IrDn-JUGTy6hT1qIjMTZOwn-r1_wNzOkiOyR-LYymTWxXeXIy040XCG35-249dvTOmT0I'
+            user: '',
+            clientId: '',
+            clientSecret: '',
+            refreshToken: ''
         }
     },
     
     // Google reCAPTCHA configuration
     // Add more domains if needed
     grecaptchaSecrets: {
-        [domain] : '6LdD2n8qAAAAAPZ8GFYJYv4hR5AUl2YOaZVKWoP-',
+        [domain] : '',
     },
     grecaptchaSiteKeys: {
-        [domain] : '6LdD2n8qAAAAAF2BlErmn1B4iwyy0IFGWgel7Iq2',
+        [domain] : '',
     },
 
     // Google Maps API configuration
     googleMapsApiKeys: {
-        [domain] : 'AIzaSyDZKfnYpMS7RlMLvNtHvVaZ-wCyFc98ThQ',
+        [domain] : '',
     },
 };
 
