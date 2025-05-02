0ea73fc3b68675582abf43c280d37db92491fe48
Tuan Kiet Nguyen <ngntuankiet@gmail.com>
2025-05-02 10:08:01 +0200

    update mod, remove not unused routes

--- server/routes/mod/index.js
+++ server/routes/mod/index.js
@@ -3,9 +3,6 @@ const { auth, cache } = require('../../middlewares');
 
 module.exports = function (app, apiPrefix) {
     app.use(apiPrefix, cache.noCache);
-
-    app.use(apiPrefix + '/auth', require('../auth/auth'));
-
     app.use(apiPrefix, auth.ensureAuthenticated);
     app.use(apiPrefix + '/retail', require('./retail'));
     app.use(apiPrefix + '/register', require('./register'));
